e513330ca2e23f95b45f2a580fe78a5d3d27505a
Veronika Nevařilová <xnevar00@stud.fit.vutbr.cz>
2023-03-06 20:15:55 +0100

    Added --help, code divided into 2 classes

--- parse.php
+++ parse.php
@@ -1,56 +1,18 @@
 <?php
 
-function xmlStart(XMLWriter $xml)
-{
-  $xml->openUri('php://stdout');
-  $xml->setIndent(2);
-  $xml->startDocument("1.0", "UTF-8");
-  $xml->startElement("program");
-  $xml->startAttribute("language");
-  $xml->text("IPPcode23");
-  $xml->endAttribute();
-}
+class Parser {
+  public $instruction_counter;
 
-function xmlEnd(XMLWriter $xml)
-{
-  $xml->endElement();
-  $xml->endDocument();
-}
-
-function errorExit($message, $error_code)
+  function errorExit($message, $error_code)
 {
   fwrite(STDERR, $message);
   exit($error_code);
-}
-function writeInstruction($instruction_counter, $xml, $word)
-{ $xml->startElement("instruction");
-  $xml->startAttribute("order");
-  $xml->text($instruction_counter);
-  $xml->endAttribute();
-  $xml->startAttribute("opcode");
-  $xml->text($word);
-  $xml->endAttribute();
-}
-
-function endInstruction($xml)
-{
-  $xml->endElement();
-}
-
-function writeElement($arg, $type, $text, $xml)
-{
-    $xml->startElement($arg);
-    $xml->startAttribute("type");
-    $xml->text($type);
-    $xml->endAttribute();
-    $xml->text($text);
-    $xml->endElement();
 }
 
 function argumentCountCheck($words, $expected_number)
 {
   if (count($words) != $expected_number)
-    errorExit("Spatny pocet operandu!\n", 23);
+    $this->errorExit("Spatny pocet operandu!\n", 23);
 }
 
 function labelCheck($word)
@@ -106,12 +68,12 @@ function constCheck($word)
     case "int":
       $word = substr($word, strlen("int")+1); 
       if(!preg_match('/^[+|-]?([1-9][0-9]*(_[0-9]+)*)|0|(0[xX][0-9a-fA-F]+(_[0-9a-fA-F]+)*)|(0[oO]?[0-7]+(_[0-7]+)*)$/', $word))
-        errorExit("Regex error!\n", 23);
+        $this->errorExit("Regex error!\n", 23);
       break;
     case "string":
       $word = substr($word, strlen("string")+1); 
       if(!preg_match('/^(?:[^\s#\\\\]|\\\\(?:\d\d\d))*$/', $word))
-        errorExit("Regex error!\n", 23);
+        $this->errorExit("Regex error!\n", 23);
       break;
     case "bool":
       $word = substr($word, strlen("bool")+1); 
@@ -126,132 +88,205 @@ function constCheck($word)
         $ok = 0;
       break;
     default:
-      errorExit("Neznamy typ!\n", 23);
+    $this->errorExit("Neznamy typ!\n", 23);
   }
   return $ok;
 }
 
 function parseNoArgs($words, $xml)
 {
-  argumentCountCheck($words, 1);
+  $this->argumentCountCheck($words, 1);
 }
 
 function parseVar($words, $xml)
 {
-  argumentCountCheck($words, 2);
+  $this->argumentCountCheck($words, 2);
 
-  if (varCheck($words[1]) == 1)
-    writeElement("arg1", "var", $words[1], $xml);
+  if ($this->varCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "var", $words[1]);
   else
-    errorExit("Argument neni var!\n", 23);
+  $this->errorExit("Argument neni var!\n", 23);
 }
 
 function parseVarSymb($words, $xml)
 {
-  argumentCountCheck($words, 3);
+  $this->argumentCountCheck($words, 3);
 
-  if (varCheck($words[1]) == 1)
-    writeElement("arg1", "var", $words[1], $xml);
+  if ($this->varCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "var", $words[1]);
   else
-    errorExit("Argument neni var!\n", 23);
+  $this->errorExit("Argument neni var!\n", 23);
 
-  if (varCheck($words[2]) == 1)
-    writeElement("arg2", "var", $words[2], $xml);
-  else if (constCheck($words[2]) == 1)
-    writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1), $xml);
+  if ($this->varCheck($words[2]) == 1)
+    $xml->writeElement("arg2", "var", $words[2]);
+  else if ($this->constCheck($words[2]) == 1)
+    $xml->writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 }
 
 function parseVarSymbSymb($words, $xml)
 {
-  argumentCountCheck($words, 4);
+  $this->argumentCountCheck($words, 4);
 
-  if (varCheck($words[1]) == 1)
-    writeElement("arg1", "var", $words[1], $xml);
+  if ($this->varCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "var", $words[1]);
   else
-    errorExit("Argument neni var!\n", 23);
+  $this->errorExit("Argument neni var!\n", 23);
 
-  if (varCheck($words[2]) == 1)
-    writeElement("arg2", "var", $words[2], $xml);
-  else if (constCheck($words[2]) == 1)
-    writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1), $xml);
+  if ($this->varCheck($words[2]) == 1)
+    $xml->writeElement("arg2", "var", $words[2]);
+  else if ($this->constCheck($words[2]) == 1)
+    $xml->writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 
-  if (varCheck($words[3]) == 1)
-    writeElement("arg3", "var", $words[3], $xml);
-  else if (constCheck($words[3]) == 1)
-    writeElement("arg3", strtok($words[3], '@'), substr($words[3], strpos($words[3], "@") + 1), $xml);
+  if ($this->varCheck($words[3]) == 1)
+    $xml->writeElement("arg3", "var", $words[3]);
+  else if ($this->constCheck($words[3]) == 1)
+    $xml->writeElement("arg3", strtok($words[3], '@'), substr($words[3], strpos($words[3], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 }
 
 function parseLabel($words, $xml)
 {
-  argumentCountCheck($words, 2);
+  $this->argumentCountCheck($words, 2);
   
-  if(labelCheck($words[1]) == 1)
-    writeElement("arg1", "label", $words[1], $xml);
+  if($this->labelCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "label", $words[1]);
   else
-    errorExit("Argument neni typu label!\n", 23);
+  $this->errorExit("Argument neni typu label!\n", 23);
 }
 
 function parseSymb($words, $xml)
 {
-  argumentCountCheck($words, 2);
+  $this->argumentCountCheck($words, 2);
 
-  if (varCheck($words[1]) == 1)
-    writeElement("arg1", "var", $words[1], $xml);
-  else if (constCheck($words[1]) == 1)
-    writeElement("arg1", strtok($words[1], '@'), substr($words[1], strpos($words[1], "@") + 1), $xml);
+  if ($this->varCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "var", $words[1]);
+  else if ($this->constCheck($words[1]) == 1)
+    $xml->writeElement("arg1", strtok($words[1], '@'), substr($words[1], strpos($words[1], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 }
 
 function parseVarType($words, $xml)
 {
-  argumentCountCheck($words, 3);
+  $this->argumentCountCheck($words, 3);
 
-  if (varCheck($words[1]) == 1)
-    writeElement("arg1", "var", $words[1], $xml);
+  if ($this->varCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "var", $words[1]);
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 
   if (strcmp($words[2], "int") == 0 || strcmp($words[2], "string") == 0 || strcmp($words[2], "bool") == 0)
-    writeElement("arg2", "type", $words[2], $xml);
+    $xml->writeElement("arg2", "type", $words[2]);
   else
-    errorExit("Argument neni type!\n", 23);
+  $this->errorExit("Argument neni type!\n", 23);
 }
 
 function parseLabelSymbSymb($words, $xml)
 {
-  argumentCountCheck($words, 4);
+  $this->argumentCountCheck($words, 4);
   
-  if(labelCheck($words[1]) == 1)
-    writeElement("arg1", "label", $words[1], $xml);
+  if($this->labelCheck($words[1]) == 1)
+    $xml->writeElement("arg1", "label", $words[1]);
   else
-    errorExit("Argument neni label!\n", 23);
+  $this->errorExit("Argument neni label!\n", 23);
 
-  if (varCheck($words[2]) == 1)
-    writeElement("arg2", "var", $words[2], $xml);
-  else if (constCheck($words[2]) == 1)
-    writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1), $xml);
+  if ($this->varCheck($words[2]) == 1)
+    $xml->writeElement("arg2", "var", $words[2]);
+  else if ($this->constCheck($words[2]) == 1)
+    $xml->writeElement("arg2", strtok($words[2], '@'), substr($words[2], strpos($words[2], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+  $this->errorExit("Argument neni symb!\n", 23);
 
-  if (varCheck($words[3]) == 1)
-    writeElement("arg3", "var", $words[3], $xml);
-  else if (constCheck($words[3]) == 1)
-    writeElement("arg3", strtok($words[3], '@'), substr($words[3], strpos($words[3], "@") + 1), $xml);
+  if ($this->varCheck($words[3]) == 1)
+    $xml->writeElement("arg3", "var", $words[3]);
+  else if ($this->constCheck($words[3]) == 1)
+    $xml->writeElement("arg3", strtok($words[3], '@'), substr($words[3], strpos($words[3], "@") + 1));
   else
-    errorExit("Argument neni symb!\n", 23);
+    $this->errorExit("Argument neni symb!\n", 23);
+}
+}
+
+class Xml {
+  public $xml;
+
+function xmlStart()
+{
+  $this->xml = new XMLWriter();
+  $this->xml->openUri('php://stdout');
+  $this->xml->setIndent(2);
+  $this->xml->startDocument("1.0", "UTF-8");
+  $this->xml->startElement("program");
+  $this->xml->startAttribute("language");
+  $this->xml->text("IPPcode23");
+  $this->xml->endAttribute();
+}
+
+function xmlEnd()
+{
+  $this->xml->endElement();
+  $this->xml->endDocument();
+}
+
+function writeInstruction($instruction_counter, $word)
+{ $this->xml->startElement("instruction");
+  $this->xml->startAttribute("order");
+  $this->xml->text($instruction_counter);
+  $this->xml->endAttribute();
+  $this->xml->startAttribute("opcode");
+  $this->xml->text($word);
+  $this->xml->endAttribute();
+}
+function endInstruction()
+{
+  $this->xml->endElement();
+}
+
+function writeElement($arg, $type, $text)
+{
+    $this->xml->startElement($arg);
+    $this->xml->startAttribute("type");
+    $this->xml->text($type);
+    $this->xml->endAttribute();
+    $this->xml->text($text);
+    $this->xml->endElement();
+}
 }
 
 $header = 0;
-$instruction_counter = 1;
-$xml = new XMLWriter();
-xmlStart($xml);
+$parser = new Parser();
+$parser->instruction_counter = 1;
+$xml = new Xml();
+
+if ($argc > 2)
+{
+  $parser->errorExit("Prilis mnoho arumentu!\n", 10);
+}
+if ($argc == 2)
+{
+  if (strcmp($argv[1], "--help") != 0)
+  {
+    $parser->errorExit("Neplatny argument!\n", 10);
+  }
+  echo "Skript nacte ze standardniho vstupu zdrojovy kod v IPPcode23, zkontroluje lexikalni a syntaktickou spravnost kodu a vypise na standardni
+vystup XML reprezentaci programu.\n
+--help    Vypise na standardni vystup napovedu skriptu\n
+Chybove navratove kody:
+21 - chybna nebo chybejici hlavicka ve zdrojovem kodu zapsanem v IPPcode23;
+22 - neznamy nebo chybny operacni kod ve zdrojovem kodu zapsanem v IPPcode23;
+23 - jina lexikalni nebo syntakticka chyba zdrojoveho kódu zapsaneho v IPPcode23.\n";
+  exit(0);
+}
+
+$header = 0;
+$parser = new Parser();
+$parser->instruction_counter = 1;
+$xml = new Xml();
+$xml->xmlStart();
 
 while(!feof(STDIN))
 {
@@ -269,30 +304,30 @@ while(!feof(STDIN))
   if ($header == 0)
   {
     if (strcmp($words[0], ".IPPcode23") != 0 || (count($words) != 1 && $words[1][0] != '#'))
-      errorExit("Spatna hlavicka!\n", 21);
+    $parser->errorExit("Spatna hlavicka!\n", 21);
     $header = 1;
     continue;
   }
 
-  writeInstruction($instruction_counter, $xml, $word);
+  $xml->writeInstruction($parser->instruction_counter, $word);
   switch ($word) {
     case "MOVE":
     case "INT2CHAR":
     case "STRLEN":
     case "TYPE":
     case "NOT":
-      parseVarSymb($words, $xml);
+      $parser->parseVarSymb($words, $xml);
       break;
     case "CREATEFRAME":
     case "PUSHFRAME":
     case "POPFRAME":
     case "RETURN":
     case "BREAK":
-      parseNoArgs($words, $xml);
+      $parser->parseNoArgs($words, $xml);
       break;
     case "DEFVAR":
     case "POPS":
-      parseVar($words, $xml);
+      $parser->parseVar($words, $xml);
       break;
     case "ADD":
     case "SUB":
@@ -307,31 +342,32 @@ while(!feof(STDIN))
     case "CONCAT":
     case "GETCHAR":
     case "SETCHAR":
-      parseVarSymbSymb($words, $xml);
+      $parser->parseVarSymbSymb($words, $xml);
       break;
     case "LABEL";
     case "JUMP":
     case "CALL":
-      parseLabel($words, $xml);
+      $parser->parseLabel($words, $xml);
       break;
     case "PUSHS":
     case "WRITE":
     case "EXIT":
     case "DPRINT":
-      parseSymb($words, $xml);
+      $parser->parseSymb($words, $xml);
       break;
     case "READ":
-      parseVarType($words, $xml);
+      $parser->parseVarType($words, $xml);
       break;
     case "JUMPIFEQ":
     case "JUMPIFNEQ":
-      parseLabelSymbSymb($words, $xml);
+      $parser->parseLabelSymbSymb($words, $xml);
       break;
     default:
-      errorExit("Neznama operace\n", 22);
+    $parser->errorExit("Neznama operace\n", 22);
   }
-  endInstruction($xml);
-  $instruction_counter++;
+  $xml->endInstruction();
+  $parser->instruction_counter++;
 }
-xmlEnd($xml);
+$xml->xmlEnd();
+exit(0);
 ?>
